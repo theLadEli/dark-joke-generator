1dd181e7873e5999022d48df94921d3cf5c8423c
Eli <109231330+theLadEli@users.noreply.github.com>
2023-04-28 15:01:40 +0100

    Finished functionality and startted styling

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 
 async function getJoke() {
-    let response = await fetch('https://v2.jokeapi.dev/joke/Dark?type=single');
+    let response = await fetch('https://v2.jokeapi.dev/joke/Dark?blacklistFlags=nsfw,racist&type=single');
     let data = await response.json();
     let joke = data.joke;
     console.log("Joke:", joke);
     document.getElementById("joke").innerText = joke;
 }
-getJoke();
+// getJoke();
